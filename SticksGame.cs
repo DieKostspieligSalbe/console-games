4f863cae95c12293636afa3fe0b3af78c7fc00e4
Alexandra Mikhailova <95976055+DieKostspieligSalbe@users.noreply.github.com>
2022-01-16 01:07:47 +0300

    added sticks game

---
+++ SticksGame.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SticksGame
+{
+    public class SticksGame
+    {
+        public event Action<string> SticksAction;
+        private int sticks;
+        public bool pickedLast = false; //true if human, false if computer
+        public bool isOver = false;
+
+        public SticksGame()
+        {
+            sticks = 10;
+        }
+        public SticksGame(int stickAmount)
+        {
+            if (stickAmount < 7)
+            {
+                sticks = 7;
+            }
+            else
+            {
+                sticks = stickAmount;
+            }
+
+        }
+
+        public void MakeAMove(int sticksAmount)
+        {
+            if (sticksAmount > 3 && !pickedLast)
+            {
+                SticksAction?.Invoke($"You cannot take more than 3 sticks!");
+                return;
+            }
+            if (sticksAmount < 1 && !pickedLast)
+            {
+                SticksAction?.Invoke($"You cannot take less than 1 stick!");
+                return;
+            }
+            if (pickedLast) //computer turn
+            {
+                Random rand = new();     
+                if (sticks <= 3)
+                {
+                    sticksAmount = rand.Next(1, sticks);
+                    sticks -= sticksAmount;
+                }
+                else
+                {
+                    sticksAmount = rand.Next(1, 3);
+                    sticks -= sticksAmount;
+                }
+            }
+            else //player turn
+            {
+                if (sticksAmount >= sticks)
+                {
+                    SticksAction?.Invoke("All the sticks will be taken");
+                    sticks = 0;
+                }
+                else
+                {
+                    sticks -= sticksAmount;
+                }
+            }
+            SticksAction?.Invoke($"{sticksAmount} sticks were taken. {sticks} sticks are left");
+            pickedLast = !pickedLast;
+            SticksAction?.Invoke(PrintSticks());
+            isOver = VictoryCheck();
+        }
+
+        private bool VictoryCheck()
+        {
+            if (sticks != 0)
+            {
+                return false;
+            }
+            string winner = pickedLast ? "Machine" : "Player";
+            isOver = true;
+            SticksAction?.Invoke($"No sticks left! {winner} wins!");
+            return true;
+        }
+
+        private string PrintSticks()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < sticks; i++)
+            {
+                sb.Append("| ");
+            }
+            return sb.ToString();
+        }
+
+        public void Print()
+        {
+            SticksAction?.Invoke(PrintSticks());
+        }
+
+
+    }
+}
---
+++ SticksGame_Main.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace SticksGame
+{
+    internal class SticksGame_Main
+    {
+        static void Main(string[] args)
+        {
+            Sticks();
+        }
+
+        static void Sticks()
+        {
+            SticksGame sticks = new();
+            sticks.SticksAction += StickActionHandler;
+            Console.WriteLine("Welcum to the game of sticks!");
+            Console.WriteLine("Each turn take 1-3 sticks until they are all gone! Last one who makes a pick, loses.");
+            sticks.Print();
+            while (!sticks.isOver)
+            {
+                if (!sticks.pickedLast) 
+                {
+                    Console.WriteLine("Print the number of sticks you want to take:");
+                    bool correctSticks = int.TryParse(Console.ReadLine(), out int sticksNumber);
+                    if (!correctSticks)
+                    {
+                        Console.WriteLine("Your input was invalid, please try again");
+                        continue;
+                    }
+                    sticks.MakeAMove(sticksNumber);
+                }  
+                else
+                {
+                    sticks.MakeAMove(0);
+                }            
+            }
+            PlayAgain();
+        }
+
+        static void StickActionHandler(string message)
+        {
+            Console.WriteLine(message);
+        }
+        static bool PlayAgain()
+        {
+            Console.WriteLine("Do you want to play again?");
+            Console.WriteLine("1. Yes    2. No");
+            ConsoleKeyInfo newGameChoice = Console.ReadKey();
+            switch (newGameChoice.KeyChar)
+            {
+                case '1':
+                    Console.Clear();
+                    Sticks();
+                    return true;
+                case '2':
+                    Console.WriteLine("\nAight :(");
+                    return false;
+                default:
+                    Console.WriteLine("\nInvalid answer input");
+                    PlayAgain();
+                    return false;
+            }
+        }
+    }
+}
